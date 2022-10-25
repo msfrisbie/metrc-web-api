08a049c94f89ffbbcaa9500b07613e18d5f8292d
Matt Frisbie <msfrisbie@gmail.com>
2022-10-25 14:13:31 -0500

    cleanup

--- jest.config.js
+++ jest.config.js
@@ -9,9 +9,4 @@ module.exports = {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       './config/fileTransformer.js',
   },
-  globals: {
-    'ts-jest': {
-      tsconfig: 'tsconfig.json'
-    }
-  }
 }
